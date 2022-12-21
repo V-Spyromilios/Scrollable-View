c37ea470d1049f7556de33a057c42ef0f2b3b8e2
Evangelos Spyromilios <71439594+V-Spyromilios@users.noreply.github.com>
2022-12-21 13:28:30 +0100

    Initial Commit

---
+++ Scrollable View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scrollable View
+//
+//  Created by Evangelos Spyromilios on 21.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
