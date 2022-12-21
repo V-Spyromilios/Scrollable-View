8e416ba74d026928b0289e9d1aa452b91539403b
Evangelos Spyromilios <71439594+V-Spyromilios@users.noreply.github.com>
2022-12-21 15:31:40 +0100

    View Orientation

--- Scrollable View/ViewController.swift
+++ Scrollable View/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	@IBOutlet var scrollViewBottomConstraint: NSLayoutConstraint!
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+//		if UIDevice.current.orientation.isLandscape {
+//			scrollViewBottomConstraint.constant = 10
+//
+//		}
 	}
+	
+	override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+		super.viewWillTransition(to: size, with: coordinator)
 
-
+		if (size.width > self.view.frame.size.width){
+			scrollViewBottomConstraint.constant = 100
+		}
+		else {
+			scrollViewBottomConstraint.constant = 50
+		}
+	}
 }
 
