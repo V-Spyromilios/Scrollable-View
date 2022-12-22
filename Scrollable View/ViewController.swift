7ef4d4d0687e6a344bf9850d26571ab61d20e414
Evangelos Spyromilios <71439594+V-Spyromilios@users.noreply.github.com>
2022-12-22 12:40:35 +0100

    ScrollView Bottom Constraint set with UIDevice.current.orientation.isLandscape

--- Scrollable View/ViewController.swift
+++ Scrollable View/ViewController.swift
@@ -12,21 +12,27 @@ class ViewController: UIViewController {
 	@IBOutlet var scrollViewBottomConstraint: NSLayoutConstraint!
 	override func viewDidLoad() {
 		super.viewDidLoad()
-//		if UIDevice.current.orientation.isLandscape {
-//			scrollViewBottomConstraint.constant = 10
-//
-//		}
+		//		if UIDevice.current.orientation.isLandscape {
+		//			scrollViewBottomConstraint.constant = 10
+		//
+		//		}
 	}
 	
 	override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
 		super.viewWillTransition(to: size, with: coordinator)
-
-		if (size.width > self.view.frame.size.width) {
+		
+		if UIDevice.current.orientation.isLandscape {
 			scrollViewBottomConstraint.constant = 100
+		} else {
+			scrollViewBottomConstraint.constant = 220
 		}
-		else {
-			scrollViewBottomConstraint.constant = 50
-		}
+			
+			//		if (size.width > self.view.frame.size.width) {
+			//			scrollViewBottomConstraint.constant = 200
+			//		}
+			//		else {
+			//			scrollViewBottomConstraint.constant = 50
+			//		}
+		
 	}
 }
-
