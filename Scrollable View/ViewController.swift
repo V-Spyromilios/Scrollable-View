93a8fddc4614e034e2661d593638e95193646585
Evangelos Spyromilios <71439594+V-Spyromilios@users.noreply.github.com>
2022-12-23 15:34:53 +0100

    Final :)

--- Scrollable View/ViewController.swift
+++ Scrollable View/ViewController.swift
@@ -8,31 +8,84 @@
 import UIKit
 
 class ViewController: UIViewController {
-	
-	@IBOutlet var scrollViewBottomConstraint: NSLayoutConstraint!
+	@IBOutlet var textView: UITextView!
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		//		if UIDevice.current.orientation.isLandscape {
-		//			scrollViewBottomConstraint.constant = 10
-		//
-		//		}
+		textView.delegate = self
+		self.textView.clipsToBounds = true
+		self.textView.sizeToFit()
 	}
 	
-	override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-		super.viewWillTransition(to: size, with: coordinator)
-		
-		if UIDevice.current.orientation.isLandscape {
-			scrollViewBottomConstraint.constant = 100
-		} else {
-			scrollViewBottomConstraint.constant = 220
+	var isOversised: Bool = false {
+		didSet {
+			textView.isScrollEnabled = isOversised
+			textView.setNeedsUpdateConstraints()
 		}
-			
-			//		if (size.width > self.view.frame.size.width) {
-			//			scrollViewBottomConstraint.constant = 200
-			//		}
-			//		else {
-			//			scrollViewBottomConstraint.constant = 50
-			//		}
-		
 	}
 }
+
+
+extension ViewController: UITextViewDelegate {
+	func textViewDidChange(_ textView: UITextView) {
+		
+		let maxHeight 	= view.frame.size.height - 180
+		isOversised =  textView.contentSize.height >= maxHeight
+	}
+	
+}
+
+
+
+
+
+
+//
+//		let estimatedSize 	= textView.sizeThatFits(CGSize(width: textView.frame.size.width, height: CGFloat.greatestFiniteMagnitude))
+//
+//
+//		textView.frame.size = CGSize(width: max(estimatedSize.width, size), height: estimatedSize.height)
+//		textView.translatesAutoresizingMaskIntoConstraints = true
+//textView.sizeToFit()
+//
+
+//
+//		//		textView.constraints.forEach { (constraint) in
+//		//			if constraint.firstAttribute == .height {
+//		//				constraint.constant = estimatedSize.height
+//		//			}
+//		//		}
+//	}
+
+
+
+
+//class ViewController: UIViewController {
+//
+//	@IBOutlet var scrollViewBottomConstraint: NSLayoutConstraint!
+//	override func viewDidLoad() {
+//		super.viewDidLoad()
+//		//		if UIDevice.current.orientation.isLandscape {
+//		//			scrollViewBottomConstraint.constant = 10
+//		//
+//		//		}
+//	}
+//}
+
+//	override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+//		super.viewWillTransition(to: size, with: coordinator)
+//
+//		if UIDevice.current.orientation.isLandscape {
+//			scrollViewBottomConstraint.constant = 100
+//		} else {
+//			scrollViewBottomConstraint.constant = 220
+//		}
+//
+//			//		if (size.width > self.view.frame.size.width) {
+//			//			scrollViewBottomConstraint.constant = 200
+//			//		}
+//			//		else {
+//			//			scrollViewBottomConstraint.constant = 50
+//			//		}
+//
+//	}
+
